f1f0a1a310ba166a6206591691a39a340c40813a
言覃 <mr.tanchao@qq.com>
2024-07-17 14:12:42 +0800

    页面配置

--- metadata/index.ts
+++ metadata/index.ts
@@ -1,13 +1,13 @@
 /** 文本 */
 export const siteName = 'Nólëbase'
 export const siteShortName = 'Nólëbase'
-export const siteDescription = '记录回忆，知识和畅想的地方'
+export const siteDescription = '言覃的个人知识库，记录 & 分享个人碎片化、结构化、体系化的技术知识内容。'
 
 /** 文档所在目录 */
 export const include = ['笔记', '生活']
 
 /** Repo */
-export const githubRepoLink = 'https://github.com/nolebase/nolebase'
+export const githubRepoLink = 'https://github.com/Mr-TChao'
 /** Discord */
 export const discordLink = 'https://discord.gg/XuNFDcDZGj'
 
